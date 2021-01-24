e56c0c80b5c52883c4119c738b7cef42a574fc01
Muhannad Al-Khatib <muhannad@simplify9.com>
2021-01-24 19:06:28 +0200

    git ignore

--- SW.Scheduler.Sdk/obj/Debug/netstandard2.1/SW.Scheduler.Sdk.AssemblyInfo.cs
+++ SW.Scheduler.Sdk/obj/Debug/netstandard2.1/SW.Scheduler.Sdk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("SimplyWorks.Pmm.Scheduler")]
+[assembly: System.Reflection.AssemblyProductAttribute("SimplyWorks.Scheduler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SW.Scheduler.Sdk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
